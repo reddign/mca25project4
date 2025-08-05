45cb86b6acb98d6dfd1c531f90034520b8be3a5c
cruz1MCA <cruz1@etown.edu>
2025-08-05 11:17:16 -0400

    leaderboard db should be working now, when the score works, un-comment the line that sets the score variable to the score

--- score.php
+++ score.php
@@ -1,15 +1,13 @@
 
 
 <!-- commented out for now so it doesnt break anything if it doesnt work -->
-
+<form action="score.php" method="post">
+        <label for="username">Username:</label>
+        <input type="text" id="uname" name="uname" required><br><br>
 <?php
 
 session_start();
 require_once("dbcreds.php");
-//<input type="text" value="$_POST['uname']">;
-$uname = '<input type="text" name="uname" value="Username?">';
-$score = $_POST["score"];
-
 
 if($_SERVER['HTTP_HOST']=="127.0.0.1"){
     $mysqli = new mysqli("127.0.0.1","root","","test");
@@ -21,15 +19,30 @@ if ($mysqli->connect_errno) {
     echo "Failed to connect: ", $mysqli->connect_error;
     exit;
 }
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+$uname = $_POST['uname'];
+
+//TODO: after the score system is set up, fix the 2 lines below\\
+//$score = $_POST['score'];
+$score = 5;
 
 //builds a sql statement\\
 $sql = "INSERT INTO leaderboard 
 (user, score)
-VALUES
-'{$uname}','{$score}';";
+VALUES(
+'{$uname}','{$score}');";
 echo $sql;
 
 
 //sends the sql statement to the db\\
 $result = $mysqli -> query($sql);
+
+}else {
+    echo "Please give a username.";
+}
 ?>
+<br><br>
+<input type="submit" value="Submit">
+</form>
+<script>event.preventDefault()</script>
